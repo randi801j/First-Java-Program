802464cc8b0fe5c501822b0d85b480d949e89be5
randyluc94 <113475637+randyluc94@users.noreply.github.com>
2023-01-11 20:39:33 -0800

    Add files via upload

---
+++ First Java Program/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("My Name is Randy Luc");
+        System.out.println("I am 27 years of age");
+        System.out.println("My home town is Los Angeles, CA");
+    }
+}
